3fdf91d8222327b43f216a43ef0e62f5f6b0441b
FWolfe <FWolfe@users.noreply.github.com>
2016-12-24 10:53:45 -0800

    Create __init__.py

---
+++ freelancer/__init__.py
@@ -0,0 +1,23 @@
+# =============================================================================
+#
+#    Copyright (C) 2016  Fenris_Wolf, YSPStudios
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# =============================================================================
+
+"""
+    freelancer - Main freelancer module. Imports most other modules and contains
+    the functions for loading freelancer's ini files.
+"""
