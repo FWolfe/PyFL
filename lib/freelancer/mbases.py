e17ae80ede43bfd7b6511cb7698a711aa4c7e607
FWolfe <FWolfe@users.noreply.github.com>
2016-12-30 10:35:27 -0800

    Added docstrings, PEP8 enforcement

--- lib/freelancer/mbases.py
+++ lib/freelancer/mbases.py
@@ -22,42 +22,55 @@
     freelancer.mbases - Helper functions for dealing with mbases.ini
 """
 from freelancer.core.data import get_group_files
-_MBASES = { }
+_MBASES = {}
 
 
 def parse_mbases():
+    """parse_mbases()
+    Parses the mbases.ini file. Due to the structure of the file, special
+    handling must be used to keep all the sections associated with the
+    correct base. This assumes the IniFile for mbases.ini has already been
+    loaded and parsed.
+    """
     mbases = get_group_files('mbases')[0]
     this_base = []
-    last_base = None    
+    last_base = None
     for section in mbases:
         if section.section == 'mbase':
             if last_base:
-                _MBASES[last_base] = MBase(this_base)            
+                _MBASES[last_base] = MBase(this_base)
             last_base = section.get('nickname').lower()
-            this_base = [ ]
+            this_base = []
         this_base.append(section)
     if last_base:
-        _MBASES[last_base] = MBase(this_base)            
-    
+        _MBASES[last_base] = MBase(this_base)
+
 def get_base(nickname):
+    """get_base(nickname)
+    Returns the MBase object for the given base nickname.
+    """
     return _MBASES[nickname]
 
 
 
 class MBase(object):
-    sections = None
+    """MBase(objects)
+    A class represnting a base's data sections in the mbases.ini file.
+    These are created when parse_mbases() is called, so is not normally called
+    manually. The inital 'objects' argument is a list of IniSection objects.
+    """
+    objects = None
     mbase = None
     factions = None
     missions = None
     rooms = None
     npcs = None
     def __init__(self, objects):
-        #raw = getEntries(nickname)
         self.objects = objects
         self.mbase = objects[0]
-        self.factions = [ ]
-        self.rooms = [ ]
-        self.npcs = [ ]
+        self.factions = []
+        self.rooms = []
+        self.npcs = []
         for obj in objects:
             if obj.section == 'mvendor':
                 self.missions = obj
@@ -69,25 +82,36 @@ class MBase(object):
                 self.rooms.append(obj)
 
     def get_vendors(self):
-        results =[ ]
-        for obj in self.npcs:
-            if '_fix_' in obj.get('nickname'):
-                results.append(obj)
-        return results
-    
+        """MBase.get_vendors()
+        Returns a list of IniSection [gf_npc] objects if the nickname contains
+        '_fix_' (assumes the npc is a vendor)
+        """
+        return [obj for obj in self.npcs if '_fix_' in obj.get('nickname')]
+
     def get_factions(self):
-        names = []
-        for obj in self.factions:
-            names.append(obj.get('faction'))
-        return names
+        """MBase.get_factions()
+        Returns a list of all [BaseFaction] 'faction' values.
+        """
+        return [obj.get('faction') for obj in self.factions]
 
     def local_faction(self):
+        """MBase.local_faction()
+        Returns the base's local_faction nickname
+        """
         return self.mbase.get('local_faction')
 
     def is_base_faction(self, faction):
+        """MBase.is_base_faction(self, faction)
+        Returns True if the faction nickname is listed as one of the base's
+        [BaseFaction] sections.
+        """
         return faction in self.get_factions()
 
     def get_faction_npcs(self, faction):
+        """MBase.get_faction_npcs(faction)
+        Returns a list of IniSection objects of all npcs affiliated with the
+        the specified faction.
+        """
         npcs = []
         for obj in self.npcs:
             if obj.get('affiliation').lower() == faction:
