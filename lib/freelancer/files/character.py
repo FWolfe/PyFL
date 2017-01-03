2e7083c6947cf3bbb8f4a1e6a7367bbd67002c61
FWolfe <FWolfe@users.noreply.github.com>
2017-01-03 11:06:27 -0800

    PEP8 enforcement, docstrings, minor refactoring

--- lib/freelancer/files/character.py
+++ lib/freelancer/files/character.py
@@ -18,16 +18,16 @@
 #
 # =============================================================================
 
+"""
+    freelancer.files.character - Helper Functions for dealing with character
+    files.
+"""
 
-# pylint: disable=C0301
-# pylint: disable=C0103
-
-from os.path import join, basename, dirname, exists
+from os.path import join, basename, dirname
 from collections import namedtuple
 from freelancer.core.regex import LINE_SPLIT_RE
 from freelancer.core import log
 from freelancer.core.settings import general
-#import freelancer.func as func
 import freelancer.files as files
 Sections = namedtuple('Sections', ('header', 'rep', 'info', 'missions'))
 KEYORDER = """
@@ -102,80 +102,75 @@ class CharFileError(Exception):
     def __init__(self, message, path, nolog=False):
         if nolog:
             return
-        log.warn("Character File: %s file:" % (message, path))
+        log.warn("Character File: %s file: %s" % (message, path))
 
-class CharFile(object):
+class CharFile(dict):
     """CharFile(accountdir, filename)
     Class representing a multiplayer character file. Unlike other files, character
     files are not automatically read and parsed when creating the object. Allowing
     the option of full reading, or just parsing/decoding the character's name.
     """
     def __init__(self, accountdir, filename):
+        dict.__init__(self)
         self.path = join(general['mp_account_path'], accountdir, filename)
         self.accountdir = accountdir
         self.charfile = filename
-        self.raw = None
-        self.data = None
+        self.lines = None
 
-    def isEncrypted(self):
-        """CharFile.isEncrypted()
+    def is_encrypted(self):
+        """CharFile.is_encrypted()
         Returns True or False if the character file is in encrypted format.
         """
-        if self.raw[0:4] == 'FLS1':
+        if self.lines[0].startswith('FLS1'):
             return True
         return False
 
-    def hasLogged(self):
-        """CharFile.hasLogged()
+    def has_logged(self):
+        """CharFile.has_logged()
         Returns True or False if the player has actually logged, or just created the
         character.
         """
-        lines = self.raw.split('\n', 3)
-        if lines[2].startswith('name ='):
+        if self.lines[2].startswith('name ='):
             return False
         return True
 
 
     def read(self, parse=True, nolog=False):
-        """CharFile.read()
+        """CharFile.read(parse=True, nolog=False)
         Reads the character file. If parse is True will also parse it.
         """
-        fi = open(self.path)
-        lines = fi.read()
-        fi.close()
+        fih = open(self.path)
+        lines = fih.read()
+        fih.close()
 
-        self.raw = lines
+        self.lines = lines.splitlines()
 
-        if self.isEncrypted():
+        if self.is_encrypted():
             raise CharFileError("Encrypted file, refusing to read", self.path, nolog)
 
-        if not self.hasLogged():
+        if not self.has_logged():
             raise CharFileError("Character never logged, refusing to read", self.path, nolog)
 
         if parse:
             self.parse()
 
 
-    def parseNameOnly(self):
-        """CharFile.parseNameOnly()
-        Parses only the description line in the character file and returns it.
-        """
-        lines = self.raw.split('\n', 6)
-        return decodeName(lines[5][7:])
 
-
-    def parse(self):
+    def parse(self, name_only=False):
         """CharFile.parse()
         Parses the full data in the character file.
         """
-        lines = self.raw.split('\n')
-        data = {}
-        for line in lines:
+        self.clear()
+        if name_only:
+            return decode_name(self.lines[5][7:])
+
+        for line in self.lines:
             if line is '':
                 continue
             match = LINE_SPLIT_RE.match(line)
             if not match:
                 continue
+
             key, value, comments = match.groups()
             if comments is None:
                 comments = ''
@@ -183,122 +178,140 @@ class CharFile(object):
                 value = ''
             string = "%s%s" % (value, comments)
             try:
-                if isinstance(data[key], str):
-                    data[key] = [data[key], string]
+                if isinstance(self[key], str):
+                    self[key] = [self[key], string]
                 else:
-                    data[key].append(string)
+                    self[key].append(string)
             except KeyError:
-                data[key] = string
+                self[key] = string
             if not key in KEYORDER:
                 log.warn("Character File: Unknown key '%s' in file %s" % (key, self.path))
-        self.data = data
 
 
     def write(self):
         """CharFile.write()
         Writes updated data to the character file.
         """
-        fi = open(self.path, 'w')
+        fih = open(self.path, 'w')
         for key in KEYORDER:
             if key is '':
-                fi.write('\n')
+                fih.write('\n')
                 continue
 
             if key[0] == '[':
-                fi.write('%s\n' % key)
+                fih.write('%s\n' % key)
                 continue
 
-            value = self.data.get(key)
+            value = self.get(key)
             if value is None:
                 continue
             elif isinstance(value, (str, unicode, int, float)):
-                fi.write('%s = %s\n' % (key, value))
+                fih.write('%s = %s\n' % (key, value))
             elif isinstance(value, list):
-                for v in value:
-                    fi.write('%s = %s\n' % (key, v))
+                for val in value:
+                    fih.write('%s = %s\n' % (key, val))
             else:
                 log.warn("Character File (write): invalid data type: %s %s" %
                          (type(value), repr(value)))
+        fih.close()
 
-        fi.close()
 
-    def getShip(self):
-        return self.data.get('ship_archetype', '')
-        
-    def getAffiliation(self):
-        return self.data.get('rep_group', '')
-        
-    def getRep(self, faction):
-        return float(self.getAllReps()[faction])
-        
-    def getAllReps(self):
+    def get_ship(self):
+        """CharFile.get_ship()
+        Proxy function for self.get('ship_archetype', '')
+        """
+        return self.get('ship_archetype', '')
+
+
+    def get_affiliation(self):
+        """CharFile.get_affiliation()
+        Proxy function for self.get('rep_group', '')
+        """
+        return self.get('rep_group', '')
+
+
+    def get_rep(self, faction):
+        """CharFile.get_rep()
+        Proxy function for self.get_reps()[faction]
+        """
+        return self.get_reps()[faction]
+
+
+    def get_reps(self):
+        """CharFile.get_reps()
+        Returns a dict of {faction_name : float} key values. The 'house' keys
+        in the file.
+        """
         results = {}
-        factions = self.data.get('house')
+        factions = self.get('house')
         for line in factions:
             rep, faction = line.split(', ', 1)
             results[faction] = float(rep)
         return results
 
-    def syncBase(self):
-        """CharFile.syncBase()
+
+    def sync_base(self):
+        """CharFile.sync_base()
         Copies the current base data and ship to the last_base keys. If the
-        player isnt docked nothing is done.
+        player isnt docked nothing is done. This is mostly useful for bugfixing.
         """
-        data = self.data
-        if not data.get('base'):
+        if not self.get('base'):
             return
-        data['lastbase'] = data['base']
-        if data.get('equip'):
-            data['base_equip'] = data['equip']
-        elif data.get('base_equip'):
-            del data['base_equip']
-
-        if data.get('cargo'):
-            data['base_cargo'] = data['cargo']
-        elif data.get('base_cargo'):
-            del data['base_cargo']
-
-        if data.get('hull_status'):
-            data['base_hull_status'] = data['hull_status']
-        elif data.get('base_hull_status'):
-            del data['base_hull_status']
-
-        if data.get('collision_group'):
-            data['base_collision_group'] = data['collision_group']
-        elif data.get('base_collision_group'):
-            del data['base_collision_group']
+        self['lastbase'] = self['base']
+        _get_or_del(self, 'equip', 'base_equip')
+        _get_or_del(self, 'cargo', 'base_cargo')
+        _get_or_del(self, 'hull_status', 'base_hull_status')
+        _get_or_del(self, 'collision_group', 'base_collision_group')
 
 
-def decodeName(name):
-    """decodeName(name)
+def _get_or_del(this_dict, src, dest):
+    """if src exits in this_dict, copy it to dest, else del dest from dict"""
+    if this_dict.get(src):
+        this_dict[dest] = this_dict[src]
+    elif this_dict.get(dest):
+        del this_dict[dest]
+
+
+def decode_name(name):
+    """decode_name(name)
     Decodes the characters name from the 'description' key in the player file.
     Returns a string.
     """
     return ''.join([name[i+2:i+4] for i in xrange(0, len(name), 4)]).decode('hex')
 
 
-def findCharFile(name, account=None):
-    """findCharFile(name, account=None)
+def find(name, account=None):
+    """find(name, account=None)
     Finds the character file for the given name and account directory. If account is
     None, checks all player accounts.
     Returns a CharFile() object.
     """
     path = general['mp_account_path']
-    if account:
-        path = join(path, account)
-    cfiles = [f for f in files.list_directory(path) if f.endswith('.fl')]
+    for fl_file in list_all(account):
+        full_path = join(path, fl_file)
+        accountdir = basename(dirname(full_path))
+        filename = basename(full_path)
 
-    for fi in cfiles:
-        np = join(path, fi)
-        accountdir = basename(dirname(np))
-        filename = basename(np)
-
-        cf = CharFile(accountdir, filename)
+        char = CharFile(accountdir, filename)
         try:
-            cf.read(parse=False, nolog=True)
+            char.read(parse=False, nolog=True)
         except CharFileError:
             continue
-        if cf.parseNameOnly() == name:
-            return cf
+        if char.parse(name_only=True) == name:
+            return char
 
 
+def list_all(account=None):
+    r"""list_all(account=None)
+    lists all .fl files in the specified account. If account is None, returns a
+    list of account\filename.fl
+    """
+    path = general['mp_account_path']
+    if account:
+        path = join(path, account)
+    return [f for f in files.list_directory(path) if f.endswith('.fl')]
+
+
+def list_encrypted():
+    """get_all_encrypted()
+    """
